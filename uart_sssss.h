20a298fc02d8afa3d62ff586ceba07ec05a7941a
krisi.qyra <krisiqyra8@gmail.com>
2023-08-13 06:47:18 -0700

    GPIO drivers were added

--- uart_sssss.h
+++ uart_sssss.h
@@ -3,6 +3,9 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+void pa_0_EXTI_Init(void);
+void uart2_transmit(int ch);
+void uart2_tx_init(void);
 //////////////////////////////////////////
 //////////////////////////////////////////
 #endif
